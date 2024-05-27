de23b7f7234bf1dd7effffe22cbe080671b39d0d
Daniele Mancinelli <mancinelli.daniele@iisgalvanimi.edu.it>
2024-05-27 08:22:26 +0000

    Aggiunte due rotte

--- verifica22_05.py
+++ verifica22_05.py
@@ -22,5 +22,41 @@ def clienti():
     info = df[df['City'] == citta]
     return render_template('table.html', tabella=info.to_html())
 
+@app.route('/elimina_cliente', methods=['GET', 'POST'])
+def elimina_cliente():
+    global df
+    msg = ''
+    if request.method == 'POST':
+        customer_id = int(request.form['customer_id'])
+        if customer_id in df['CustomerID'].values:
+            df = df[df['CustomerID'] != customer_id]
+            df.to_csv('/workspace/Verifica22-05/data/vendite.csv', index=False)
+            msg = 'Cliente eliminato.'
+        else:
+            msg = 'Cliente inesistente.'
+    
+    return render_template('elimina_cliente.html', msg=msg) 
+
+@app.route('/aggiungi_cliente', methods=['GET', 'POST'])
+def aggiungi_cliente():
+    global df
+    msg = ''
+    if request.method == 'POST':
+        new_row = {
+            'CustomerID': int(request.form['customer_id']),
+            'CustomerName': request.form['customer_name'],
+            'ContactName': request.form['contact_name'],
+            'Address': request.form['address'],
+            'City': request.form['city'],
+            'PostalCode': request.form['postal_code'],
+            'Country': request.form['country']
+        }
+        new_df = pd.DataFrame([new_row])
+        df = pd.concat([df, new_df], ignore_index=True)
+        df.to_csv('/workspace/Verifica22-05/data/vendite.csv', index=False)
+        msg = 'Cliente aggiunto con successo!'
+    
+    return render_template('aggiungi_cliente.html', msg=msg)
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
